cff34817ab028ae512685d9af11154ef2d5da7a7
Aman Raj <amanraj06042001@gmail.com>
2026-01-10 00:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let totalCost = 0;
+
+  while (arr.length > 1) {
+    // sort array
+    arr.sort((a, b) => a - b);
+
+    // take two smallest
+    let first = arr.shift();
+    let second = arr.shift();
+
+    let cost = first + second;
+    totalCost += cost;
+
+    // push merged rope back
+    arr.push(cost);
+  }
+
+  return totalCost;
+
 }
 
 module.exports=mincost;
